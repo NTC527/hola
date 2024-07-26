bdfc934b16f4f8e0087fa1033de66c5fe442ba67
NTC527 <thiago.calderon527@gmail.com>
2024-07-25 22:19:54 -0400

    Add files via upload

---
+++ ThiagoVentas/src/Modelo/LoginDAO.java
@@ -0,0 +1,69 @@
+
+package Modelo;
+
+import Vista.Login;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAO {
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    Conexion cn = new Conexion();
+    private login login;
+    
+    public int log(String correo, String pass) throws SQLException{
+        login = new login();
+        String sql = "SELECT * FROM usuarios WHERE correo = ? AND pass = ?";
+        {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, correo);
+            ps.setString(2, pass);
+            rs= ps.executeQuery();
+            if (rs.next()) {
+                l.setId(rs.getInt("id"));
+                l.setNombre(rs.getString("nombre"));
+                l.setCorreo(rs.getString("correo"));
+                l.setPass(rs.getString("pass"));
+                
+            }
+        }
+        return 1;
+    }
+
+    public Login login(String correo, String pass) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static class l {
+
+        private static void setId(int aInt) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        private static void setNombre(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        private static void setCorreo(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        private static void setPass(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        public l() {
+        }
+    }
+
+    private static class login {
+
+        public login() {
+        }
+    }
+}
+
